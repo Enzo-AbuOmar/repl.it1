b64edf0dbbfdf9335f588ab34111837685d7e27b
Glitch (petalite-loving-pleasure) <none>
2020-08-02 12:48:44 +0000

    🌸🍩 Checkpoint ./config.json.example:16790994/59 ./config.json.:16790994/436 ./config.json:16790994/361 ./commands/help.js:16790994/23

--- commands/help.js
+++ commands/help.js
@@ -8,9 +8,9 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle("Evobot Help")
+      .setTitle("MonsBot Help")
       .setDescription("List of all commands")
-      .setColor("#F8AA2A");
+      .setColor("#FFFFFF");
 
     commands.forEach((cmd) => {
       helpEmbed.addField(
